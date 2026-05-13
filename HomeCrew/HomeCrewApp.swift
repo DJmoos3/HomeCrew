9b81378f539eb55cc149bf736602ad5b9541702c
DJmoos3 <isaac.strandh@gmail.com>
2026-05-13 10:43:11 +0200

    First initialization

---
+++ HomeCrew/HomeCrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCrewApp.swift
+//  HomeCrew
+//
+//  Created by Isaac Strandh on 2026-05-13.
+//
+
+import SwiftUI
+
+@main
+struct HomeCrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
